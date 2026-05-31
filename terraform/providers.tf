064a0425178f3a2f1dc9823d2a87b9e17d23e4ae
PeasfulTown <peasfultown@gmail.com>
2026-05-31 13:58:40 +0700

    discard dynamodb lock table as that is deprecated

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_version = ">= 1.15.0"
 
   backend "s3" {
-    key    = "filehost/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "global-terraform-locks"
+    key          = "filehost/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
